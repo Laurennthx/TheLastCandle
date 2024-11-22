9992a91fd31b1ab3531883ef5383686e5d313c39
andremip <andremiguelperez@gmail.com>
2024-11-22 21:18:56 +0100

    Pantalla dividida con marco en medio.
    Marco de en medio arreglado creando una tercera cámara que sólo renderice el marco e ignore el resto de sprites

--- juegos en red/js/game.js
+++ juegos en red/js/game.js
@@ -20,7 +20,7 @@ class GameScene extends Phaser.Scene {
     create() {
 
         // Set debug mode for the physics engine (shows the bounding boxes)
-        this.physics.world.createDebugGraphic();
+        // this.physics.world.createDebugGraphic();
 
         // Create ball
         this.ball = this.physics.add.sprite(400, 530, 'ball');
@@ -34,12 +34,13 @@ class GameScene extends Phaser.Scene {
         this.terrain.body.setImmovable(true);
         this.terrain.body.setAllowGravity(false);
         this.terrain.setScale(0.06,0.06);
-        
 
-        // Add colliders
-        this.physics.add.collider(this.ball, this.terrain, this.hitGround, null, this); // LLama a la función "hitGround" cuando colisionan
+        // Añadir la imagen del marco en el centro de la pantalla
+        const divider = this.add.image(this.scale.width / 2, this.scale.height / 2, 'divider')
+        .setOrigin(0.5, 0.5); // Centra la imagen en ambos ejes
+        divider.setDepth(1); // Asegura que la imagen esté por encima de otros elementos
 
-
+        // Bueno esto no sirve de mucho.
         const hello_text = this.add.text(250, 350, 'Press space to start!', { fill: '#000000', fontSize: 40 });
         hello_text.setOrigin(-0.7, 1);
         this.input.keyboard.on("keydown-SPACE", () => {
@@ -50,20 +51,39 @@ class GameScene extends Phaser.Scene {
 
         });
 
+        // CAMBIAR!!! He visto que se pueden usar grupos o containers para hacer colecciones de sprites. Los grupos y containers
+        // hacen cosas distintas pero no me ha dado tiempo a mirar cómo funcionan. Serviría para que la tercera cámara ignore todo el
+        // grupo o el container. 
+        //this.backgroundGroup = this.add.group()
+        //this.backgroundGroup.add(divider)
+        //this.backgroundGroup.add(hello_text)
+        
+
+        // Add colliders
+        this.physics.add.collider(this.ball, this.terrain, this.hitGround, null, this); // LLama a la función "hitGround" cuando colisionan
+
+        
+        // Enable inputs
         this.setupPaddleControllersExorcist();
         this.setupPaddleControllersDemon();
 
+
+        // CREACIÓN DE LAS CÁMARAS:
+        // Primera cámara que sigue al exorcista
         this.cameras.main.setSize(this.scale.width / 2, this.scale.heigh)
         this.cameras.main.startFollow(this.ball)
-
+        // Segunda cámara que sigue al demonio
         const scndCamera = this.cameras.add(this.scale.width / 2, 0, this.scale.width / 2, this.scale.heigh, false, 'demonCamera')
         scndCamera.startFollow(this.terrain)
+        // Tercera cámara que sólo renderiza el borde
+        const marcoCamera = this.cameras.add(0, 0, this.scale.width, this.scale.heigh)        
 
-        // Añadir la imagen del marco en el centro de la pantalla
-        const divider = this.add.image(this.scale.width / 2, this.scale.height / 2, 'divider')
-        .setOrigin(0.5, 0.5); // Centra la imagen en ambos ejes
-        divider.setDepth(1); // Asegura que la imagen esté por encima de otros elementos
-
+        // IGNORAR SPRITES:
+        // Las cámaras de la pantalla dividida ignoran el marco
+        this.cameras.main.ignore(divider)
+        scndCamera.ignore(divider)
+        // La tercera cámara debe ignorar todos los sprites XD
+        marcoCamera.ignore([this.ball, this.terrain, hello_text])
     }    
 
     setupPaddleControllersDemon() {
