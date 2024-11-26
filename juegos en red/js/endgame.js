2ad619eed5c363cdcf2112fecce442d247b39cfb
Laurennthx <lauragarciamartin.u@gmail.com>
2024-11-26 09:28:00 +0100

    demon wins

--- juegos en red/js/endgame.js
+++ juegos en red/js/endgame.js
@@ -4,7 +4,9 @@ class EndGame extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image("endGameImage", "assets/UI/endGame.png");
+        this.load.image("endGameImage", "assets/UI/DemonWins.jpg");
+        this.load.image("tryAgain", "assets/UI/tryAgain.png");
+
     }
 
     create(data) {
@@ -13,19 +15,28 @@ class EndGame extends Phaser.Scene {
         const background = this.add.image(0,0, "endGameImage").setOrigin(0,0);
         background.setDisplaySize(1920, 1080);
 
-        const remaining_bricks = data.remaining_bricks;
+        //const remaining_bricks = data.remaining_bricks;
 
-        const message = "You lose";
+        //const message = "You lose";
 
-        const messageText = this.add.text(400, 300, message + '\nClick to restart', {
-            fontSize: '64px',
-            fill: '#000000',
-            align: 'center'
-        }).setOrigin(-0.15,-0.2);
+        //const messageText = this.add.text(400, 300, message + '\nClick to restart', {
+        //    fontSize: '64px',
+        //    fill: '#000000',
+       //     align: 'center'
+       // }).setOrigin(-0.15,-0.2);
 
         this.input.once('pointerdown', () => {
             this.scene.start("MenuScene");
         });
+
+        const returnButton = this.add.image(960, 1000, "tryAgain")
+        .setInteractive()
+        .on('pointerdown', () => {
+            this.sound.play("select");
+            this.scene.stop("GameScene");
+            this.scene.start("MenuScene");   
+        });       
+        returnButton.setScale(0.4,0.4);
 
     }
 
