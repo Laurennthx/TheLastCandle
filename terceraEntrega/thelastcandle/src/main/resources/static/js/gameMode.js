f9565a7394092a253280ecc423224f3e11ea5a88
crisstinagl <crisgdl3011@gmail.com>
2024-12-17 22:12:09 +0100

    Aviso opcion online no disponible

--- terceraEntrega/thelastcandle/src/main/resources/static/js/gameMode.js
+++ terceraEntrega/thelastcandle/src/main/resources/static/js/gameMode.js
@@ -6,6 +6,7 @@ class GameModeScene extends Phaser.Scene {
     preload() {
         this.load.audio("select", 'assets/select.mp3');
     
+        this.load.image("notAble", "assets/UI/NotAbleYet.png");
         this.load.image("PlayModeBG", "assets/UI/PlayMode.jpg");
         this.load.image("localB", "assets/UI/localB.png");
         this.load.image("onlineB", "assets/UI/onlineB.png");
@@ -35,15 +36,17 @@ class GameModeScene extends Phaser.Scene {
         .setInteractive()
         .on('pointerdown', () => {
             this.sound.play("select");
-            this.scene.stop("GameMode");
-            this.scene.start("ChoosingCharacterScene");   
+            //this.scene.stop("GameMode");
+            //this.scene.start("ChoosingCharacterScene");   
         }).on('pointerover', () => {
             this.sound.play("hover"); // Reproduce sonido al pasar el cursor
         });  
         onlineB.setScale(0.7,0.7);
     
-    
-    
+        // Imagen aviso online no disponible
+        const warning = this.add.image(1300, 675, "notAble");
+        warning.setScale(1.5,1.5);
+
     
     
     
